2a6afcdcf15797bf520bc851008b6056f79f7f89
HaseebTempi <m.haseeb@tempi-recovery.ch>
2024-09-17 14:08:35 +0500

    17 September

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "fee7315e6b54888f77e06378d4655384",
+const RESOURCES = {"flutter_bootstrap.js": "a3e9ee16426eb58b550969cf556d0680",
 "version.json": "ac41d82c9541ce3c85ce7c3cf8b371e7",
-"index.html": "41f2b5ff9a119fada6a1f572d66494fd",
-"/": "41f2b5ff9a119fada6a1f572d66494fd",
-"main.dart.js": "e28fc23f1e546651093518af4fada6aa",
+"index.html": "b36a30b20d8fd5358a5dbfcb5d356df4",
+"/": "b36a30b20d8fd5358a5dbfcb5d356df4",
+"main.dart.js": "5a8f086762d4688c7f4cf1bc76ecec4e",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
