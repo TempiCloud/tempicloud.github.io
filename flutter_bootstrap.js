c7e6114e04014aa009bf818b7e260e7f3a01b8fe
HaseebTempi <m.haseeb@tempi-recovery.ch>
2024-09-26 11:55:31 +0500

    26 september

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a6bd3f1de158bb61090e0c8053df93a10cb548
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1874602986"
+    serviceWorkerVersion: "3682391853"
   }
 });
